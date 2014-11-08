f8acc34c485084b43facdc4033cc62f15815f3b0
Eddie Obropta <eobropta@gmail.com>
2014-11-08 14:23:44 -0500

    First commit.

---
+++ main.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MCTB - DMD Project
+% Alex Mijailovic, Eddie Obropta, Whitney Young
+% Fall 2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+close all
+% Plots Data
+%% Load Data
+% Use the following file structure
+% MATLAB/dmd_project/
+% --- code/
+% --- data/
+
+load('../data/age_data.mat');
+load('../data/data.mat');
+
+%% Preprocess Data
+
+
+
+
+
